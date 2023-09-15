daf19a5b4e17a3de0e47773019d834659e7c4de3
owen <wavefunctionc@gmail.com>
2023-09-16 00:10:14 +0900

    WebSocket Config 작성

--- Access-Control/src/main/java/com/accesscontrol/config/WebSocketConfig.java
+++ Access-Control/src/main/java/com/accesscontrol/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/count");
         registry.setApplicationDestinationPrefixes("/");
     }
 
