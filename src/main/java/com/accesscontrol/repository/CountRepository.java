406077e33cd1f968a2a6ae0c4b1db88f00b01619
Kunwoo Shin <recyclefunction@gmail.com>
2023-09-17 03:03:53 +0900

    Rename Repository

--- src/main/java/com/accesscontrol/repository/CountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.accesscontrol.repository;
-
-import com.accesscontrol.entity.Count;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountRepository extends JpaRepository<Count, Long> {
-}
