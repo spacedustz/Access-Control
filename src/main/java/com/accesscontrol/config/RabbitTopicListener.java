3f063effaa91eb718b333e9e44beb6d7a7a7f7c3
KunWoo Shin <cyberspecterr@gmail.com>
2023-09-16 22:26:13 +0900

    Rebase Project - React 프로젝트 제거

--- src/main/java/com/accesscontrol/config/RabbitTopicListener.java
+++ src/main/java/com/accesscontrol/config/RabbitTopicListener.java
@@ -1,54 +1,25 @@
 package com.accesscontrol.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.accesscontrol.repository.CountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class RabbitTopicListener {
 
+    private CountRepository countRepository;
+
     @RabbitListener(queues = "test.queue")
-    public void receive(String message) {
-        log.info("Received Message: {}", message);
-        System.out.println("Received Message: " + message);
-
-        ClassPathResource resource = new ClassPathResource("sample/sample-tripwire.json");
-        List<JsonNode> rootNode = new ArrayList<>();
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        String jsonContent = null;
-        try {
-            jsonContent = Files.readString(Paths.get(resource.getURI()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        String[] splitJson = jsonContent.split("\n");
-
-        for (String json : splitJson) {
-            JsonNode jsonObject = null;
-            try {
-                jsonObject = objectMapper.readTree(json);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-            rootNode.add(jsonObject);
-        }
-
-        for (JsonNode node : rootNode) {
-
-        }
+    public void receive(Message message) {
+        log.info("Received Message: " + message.toString());
+//        System.out.println("Received Message: " + message);
+//        Count entity = Count.createOf(message.getExtra().getCount(), message.getEventTime());
+//        countRepository.save(entity);
+//        log.info("Entity", entity);
     }
 }
