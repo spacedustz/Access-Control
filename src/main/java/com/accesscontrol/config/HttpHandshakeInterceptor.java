cb2895733f817917b727050f3f8d5245e707065e
owen <wavefunctionc@gmail.com>
2023-09-19 17:56:51 +0900

    Http Handshake Interceptor 주석 추가

--- src/main/java/com/accesscontrol/config/HttpHandshakeInterceptor.java
+++ src/main/java/com/accesscontrol/config/HttpHandshakeInterceptor.java
@@ -18,12 +18,17 @@ public class HttpHandshakeInterceptor implements HandshakeInterceptor {
                                    WebSocketHandler wsHandler,
                                    Map<String, Object> attributes) throws Exception {
         if (request instanceof ServletServerHttpRequest servletRequest) {
+            // 현재 요청이 HTTP 요청인 경우, HTTP 세션을 가져옵니다.
             HttpSession session = servletRequest.getServletRequest().getSession();
+            // 가져온 HTTP 세션을 WebSocket 연결과 연관된 속성(attributes)으로 저장합니다.
             attributes.put(SESSION, session);
         }
+        // true를 반환하면 WebSocket 연결이 계속 진행되고, false를 반환하면 연결이 중단됩니다.
         return true;
     }
 
+    // WebSocket 연결이 수립된 후에 실행됩니다.
+    // 주로 예외 처리나 추가 작업을 수행하는 데 사용됩니다.
     @Override
     public void afterHandshake(ServerHttpRequest request,
                                ServerHttpResponse response,
