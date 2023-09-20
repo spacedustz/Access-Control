98702817ed1afd6fe3037cc11735b430bb47b627
owen <wavefunctionc@gmail.com>
2023-09-20 18:03:27 +0900

    JS 분리 - index, admin

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -33,18 +33,31 @@ function loadInitialData() {
             roomInfo.status = initialRoomInfo.status;
 
             console.log("초기 정보 로드");
-
-            displayStatus(roomInfo.status);
-            displayOccupancy(roomInfo.occupancy);
-            displayMaxCount(roomInfo.maxCount);
         });
 }
 
-// 현재 인원 업데이트 함수
-function updateRoomInfo(updatedData) {
-    displayStatus(updatedData.status);
-    displayOccupancy(updatedData.occupancy);
-    displayMaxCount(updatedData.maxCount);
+// Status 값 변경
+function updateStatus() {
+    let newStatusValue= document.getElementById('new-status').value;
+
+    fetchText(httpUrl + '/update-status?status=' + newStatusValue, 'PATCH', {})
+        .then(updatedStatus => {
+            console.log('업데이트 된 상태 : ', updatedStatus);
+        })
+
+    return false; // 기본 양식 제출 방지
+}
+
+// 방 최대인원 수 (Max Count) 값 변경
+function updateMaxCount() {
+    let newMaxCountValue = document.getElementById('new-max').value;
+
+    fetchJson(httpUrl + '/update-max?max=' + newMaxCountValue, 'PATCH', {})
+        .then(updatedEvent => {
+            console.log('업데이트 된 최대 인원 : ', updatedEvent.maxCount);
+        })
+
+    return false; // 기본 양식 제출 방지
 }
 
 /* --- Utility 함수 --- */
@@ -56,19 +69,4 @@ function fetchJson(url, method='GET') {
 function fetchText(url, method='PATCH', body={}) {
     return window.fetch(url,{method , headers : {'Content-Type': 'application/json'}, body : JSON.stringify(body)})
         .then(response => response.text());
-}
-
-// 방안의 현재 인원
-function displayOccupancy(occupancy) {
-    document.getElementById('count').innerText= occupancy;
-}
-
-// 방안의 상태
-function displayStatus(status) {
-    document.getElementById('status').innerText = status ;
-}
-
-// 최대 인원
-function displayMaxCount(max) {
-    document.getElementById('max').innerText = max;
 }
