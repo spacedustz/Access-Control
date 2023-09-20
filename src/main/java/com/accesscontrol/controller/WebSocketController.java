96edce773489ac6f0bdb402f46c5ce0f702bc01c
spacedustz <cyberspecterr@gmail.com>
2023-09-20 16:54:35 +0900

    간단한 Rest API용 Web Socket 컨트롤러

---
+++ src/main/java/com/accesscontrol/controller/WebSocketController.java
@@ -0,0 +1,33 @@
+package com.accesscontrol.controller;
+
+import com.accesscontrol.dto.EventDTO;
+import com.accesscontrol.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ws")
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final EventService eventService;
+
+    // Event 객체 Status 값 변경 API
+    @PatchMapping("/update-status")
+    public ResponseEntity<String> updateStatus(@RequestParam String status) {
+        return new ResponseEntity<>(eventService.updateStatus(status), HttpStatus.OK);
+    }
+
+    // Event 객체 MaxCount 값 변경 API
+    @PatchMapping("/update-max")
+    public ResponseEntity<EventDTO.Response> updateMaxCount(@RequestParam String max) {
+        return new ResponseEntity<>(EventDTO.Response.fromEntityForUpdateMaxCount(eventService.updateMaxCount(max)), HttpStatus.OK);
+    }
+
+    // 초기 데이터 로드용 API
+    @GetMapping("/init")
+    public ResponseEntity<EventDTO.Response> getInitData() {
+        return new ResponseEntity<>(EventDTO.Response.fromEntity(eventService.getInitData()), HttpStatus.OK);
+    }
+}
