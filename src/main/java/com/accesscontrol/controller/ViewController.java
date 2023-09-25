b4af9fb70160352223d3eee30cef4b6e8059c91b
owen <floralexium@gmail.com>
2023-09-25 21:23:11 +0900

    ViewController - 주석 추가

--- src/main/java/com/accesscontrol/controller/ViewController.java
+++ src/main/java/com/accesscontrol/controller/ViewController.java
@@ -3,9 +3,9 @@ package com.accesscontrol.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// 어드민 페이지 분리를 위한 컨트롤러
 @Controller
 public class ViewController {
-
     @RequestMapping("/admin")
     public String adminPage() {
         return "forward:/admin.html";
