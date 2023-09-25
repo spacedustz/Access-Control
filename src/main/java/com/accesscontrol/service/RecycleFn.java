e848e4ea99d658208bc628785c556b68550f7e05
owen <floralexium@gmail.com>
2023-09-25 21:26:30 +0900

    RecycleFn - 공통로직 몇개 추가

--- src/main/java/com/accesscontrol/service/RecycleFn.java
+++ src/main/java/com/accesscontrol/service/RecycleFn.java
@@ -1,19 +1,51 @@
 package com.accesscontrol.service;
 
 import com.accesscontrol.entity.Event;
+import com.accesscontrol.entity.Status;
+import com.accesscontrol.repository.EventRepository;
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class RecycleFn {
+
+    private final EventRepository eventRepository;
+
     public void autoUpdateStatus(Event event) {
         if (event != null) {
             if (event.getOccupancy() <= 9) {
-                event.setStatus("입장 가능합니다.");
+                event.setStatus(Status.LOW);
             } else if (event.getOccupancy() <= 14) {
-                event.setStatus("혼잡합니다.");
+                event.setStatus(Status.MEDIUM);
             } else if (event.getOccupancy() >= 15) {
-                event.setStatus("만실입니다.");
+                event.setStatus(Status.HIGH);
             }
         }
+    }
+
+    // 엔티티 수치 초기화 함수
+    public void initiateCount(Event event) {
+        event.setOccupancy(0);
+        event.setInCount(0);
+        event.setOutCount(0);
+    }
+
+    // 년-월-일 변환 함수
+    public String ymdFormatter(@Nullable LocalDateTime dateTime) {
+        DateTimeFormatter YMDFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return dateTime.format(YMDFormatter);
+    }
+
+    // 시-분 변환 함수
+    public String hmFormatter(@Nullable LocalDateTime date) {
+        DateTimeFormatter HMFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        return date.format(HMFormatter);
     }
 }
