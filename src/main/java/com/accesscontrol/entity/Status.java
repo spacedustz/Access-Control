c96803c7751edc3ada114f34ff4778fc52db2672
spacedustz <cyberspecterr@gmail.com>
2023-10-05 10:37:16 +0900

    View - Status 값, 상태 표시 이미지 png 교체

--- src/main/java/com/accesscontrol/entity/Status.java
+++ src/main/java/com/accesscontrol/entity/Status.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 // 현재 방안의 상태
 public enum Status {
     LOW("입장 가능합니다."),
-    MEDIUM("혼잡합니다."),
-    HIGH("만실입니다."),
+    MEDIUM("조금 혼잡합니다."),
+    HIGH("입장이 불가합니다."),
     NOT_OPERATING("운영시간이 아닙니다.");
 
     @Getter
