a5804d4aa16c0f72cc45bdc1d6b82cdba554253c
owen <wavefunctionc@gmail.com>
2023-10-13 20:19:14 +0900

    EventDto - Response Dto 삭제

--- src/main/java/com/accesscontrol/dto/EventDTO.java
+++ src/main/java/com/accesscontrol/dto/EventDTO.java
@@ -1,7 +1,5 @@
 package com.accesscontrol.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
@@ -20,43 +18,5 @@ public class EventDTO {
     @Getter
     public static class Extra {
         private String crossing_direction;
-    }
-
-    // Event 객체 Response 용도
-    @Getter
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class Response {
-        private Long id; // ID
-        private int occupancy; // 현재 Room 내 인원 수 : InCount - OutCount
-        private int maxCount; // 최대 수용 인원
-        private String customStatus; // Room 상태
-        private String relayUrl; // Relay URL
-        private String status; // status
-        private String openTime; // 운영시작 시간
-        private String closeTime; // 운영 종료 시간
-
-        private Response(int maxCount) {
-            this.maxCount = maxCount;
-        }
-
-        public static EventDTO.Response fromEntity(com.accesscontrol.entity.Event entity) {
-            return new EventDTO.Response(
-                    entity.getId(),
-                    entity.getOccupancy(),
-                    entity.getMaxCount(),
-                    entity.getCustomStatus(),
-                    entity.getRelayUrl(),
-                    entity.getStatus().getDesc(),
-                    entity.getOpenTime(),
-                    entity.getCloseTime()
-            );
-        }
-
-        // maxCount 업데이트 용
-        public static EventDTO.Response fromEntityForUpdateMaxCount(com.accesscontrol.entity.Event entity) {
-            return new EventDTO.Response(
-                    entity.getMaxCount()
-            );
-        }
     }
 }
