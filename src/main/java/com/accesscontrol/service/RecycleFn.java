f65204ca4e23e26dadf0dece34d25a9942bcadf6
owen <wavefunctionc@gmail.com>
2023-10-13 20:20:51 +0900

    검증 & 변환 로직 전부 RecycleFn으로 이동

--- src/main/java/com/accesscontrol/service/RecycleFn.java
+++ src/main/java/com/accesscontrol/service/RecycleFn.java
@@ -13,6 +13,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -24,6 +25,7 @@ import java.time.format.DateTimeFormatter;
 public class RecycleFn {
     private final EventRepository eventRepository;
     private final SimpMessagingTemplate template;
+    private String currentDate = String.valueOf(LocalDate.now());
 
     @Cacheable("entityCount")
     public Long getEntityCount() {
@@ -33,16 +35,6 @@ public class RecycleFn {
     @Cacheable("entity")
     public Event getEntity(Long pk) {
         return eventRepository.findById(pk).orElseThrow(() -> new CommonException("Data-001", HttpStatus.NOT_FOUND));
-    }
-
-    public void autoUpdateStatus(Event event) {
-        if (event.getOccupancy() <= 9) {
-            event.setStatus(Status.LOW);
-        } else if (event.getOccupancy() >= 10 && event.getOccupancy() < event.getMaxCount()) {
-            event.setStatus(Status.MEDIUM);
-        } else if (event.getOccupancy() >= event.getMaxCount()) {
-            event.setStatus(Status.HIGH);
-        }
     }
 
     // 엔티티 수치 초기화 함수
@@ -64,13 +56,22 @@ public class RecycleFn {
         return date.format(HMFormatter);
     }
 
-    public void validateOccupancy(Event event) {
-        if (event.getOccupancy() < 0) {
-            initiateCount(event);
-            log.error("재실 인원은 0명 이하가 될 수 없습니다. - 초기화 된 방안 인원 수치 : {}", event.getOccupancy());
+    // 현재 인원에 따른 객체 상태 업데이트
+    public void autoUpdateStatus(Event event) {
+        if (event.getCustomStatus().equals("")) {
+            if (event.getOccupancy() <= 9) {
+                event.setStatus(Status.LOW);
+            } else if (event.getOccupancy() >= 10 && event.getOccupancy() < event.getMaxCount()) {
+                event.setStatus(Status.MEDIUM);
+            } else if (event.getOccupancy() >= event.getMaxCount()) {
+                event.setStatus(Status.HIGH);
+            }
+        } else {
+            log.info("현재 방안의 상태 - {}", event.getCustomStatus());
         }
     }
 
+    // 운영 시간 검증 함수
     public Event validateOperationTime(Event event) {
         String openTime = event.getOpenTime();
         String closeTime = event.getCloseTime();
@@ -87,7 +88,7 @@ public class RecycleFn {
 
         // 운영 시간 검증
         if (now.isAfter(open) && now.isBefore(close)) {
-            log.info("정상 운영 시간 입니다. - 현재 시간 : {}", now);
+            log.info("정상 운영 시간 입니다. [ 운영 시간 ] {} - {}", open, close);
             autoUpdateStatus(event);
         } else {
             event.setStatus(Status.NOT_OPERATING);
@@ -96,5 +97,56 @@ public class RecycleFn {
         }
 
         return event;
+    }
+
+    // 재실 인원 검증 함수
+    public void validateOccupancy(Event event) {
+        try {
+            if (event.getOccupancy() < 0) {
+                initiateCount(event);
+                eventRepository.save(event);
+
+                log.error("재실 인원 오류 - In/Out Count, Occupancy 초기화 - 초기화 된 Occupancy 값 : {}", event.getOccupancy());
+            }
+
+            if (event.getOccupancy() >= event.getMaxCount()) {
+                log.info("인원 초과 - 재실 인원/최대인원 : {}명/{}명", event.getOccupancy(), event.getMaxCount());
+            }
+
+            template.convertAndSend("/count/data", event);
+        } catch (Exception e) {
+            log.error("Occupancy, In/Out Count 값 초기화 후 객체 저장 실패 - Event ID : {}", event.getId(), e);
+        }
+    }
+
+    // 운영시간 검증 함수
+    public void validateOperatingStatus(String entityYMDDate,
+                                        String eventYMDDate,
+                                        LocalTime open,
+                                        LocalTime close,
+                                        LocalTime eventDateTime,
+                                        String openTime,
+                                        String closeTime,
+                                        Event event) {
+
+        // 이벤트 데이터의 날짜 검증
+        if (!eventYMDDate.equals(currentDate) || (!entityYMDDate.equals(currentDate))) {
+            log.error("데이터의 날짜가 오늘 날짜가 아닙니다. - 현재 날짜 : {}, 데이터의 날짜 : {}", currentDate, eventYMDDate);
+        }
+
+        // 이벤트 데이터의 운영 시간 검증
+        if (!eventDateTime.isAfter(open) && !eventDateTime.isBefore(close)) {
+            event.setStatus(Status.NOT_OPERATING);
+            log.error("운영 시간이 아닙니다. - 운영 시간 : {} - {}, 입장한 시간 : {}", openTime, closeTime, eventDateTime);
+        }
+
+        try {
+            eventRepository.save(event);
+        } catch (Exception e) {
+            log.error("Occupancy, In/Out Count 값 초기화 후 객체 저장 실패 - Event ID : {}", event.getId());
+        }
+
+        // Web Socket Session에 Event 객체 전달
+        template.convertAndSend("/count/data", event);
     }
 }
