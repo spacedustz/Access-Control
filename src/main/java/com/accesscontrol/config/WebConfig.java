834b218083675d7700b4c2e5d2523825a6ad20e7
owen <wavefunctionc@gmail.com>
2023-10-14 16:07:28 +0900

    WebConfig - WebCLient & TaskExecutor Bean 추가

--- src/main/java/com/accesscontrol/config/WebConfig.java
+++ src/main/java/com/accesscontrol/config/WebConfig.java
@@ -2,7 +2,10 @@ package com.accesscontrol.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebConfig {
@@ -10,5 +13,22 @@ public class WebConfig {
     @Bean(name = "api")
     public RestTemplate template() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
+
+    @Bean
+    public TaskExecutor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(1);
+        executor.setThreadNamePrefix("Instance Thread-");
+        executor.initialize();
+
+        return executor;
     }
 }
