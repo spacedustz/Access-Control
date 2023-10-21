6e75b8a0db29ec2a34d2c34009f505bb5775348a
owen <wavefunctionc@gmail.com>
2023-10-21 20:25:15 +0900

    RabbitTopicListener - 로직 수정, PostConstruct 작성

--- src/main/java/com/accesscontrol/service/RabbitTopicListener.java
+++ src/main/java/com/accesscontrol/service/RabbitTopicListener.java
@@ -2,14 +2,19 @@ package com.accesscontrol.service;
 
 import com.accesscontrol.dto.EventDTO;
 import com.accesscontrol.entity.Event;
+import com.accesscontrol.error.CommonException;
 import com.accesscontrol.repository.EventRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -25,6 +30,11 @@ public class RabbitTopicListener {
     private final RecycleFn recycleFn;
     private final RestTemplate restTemplate;
 
+    @PostConstruct
+    public void init() throws Exception {
+        createEntity();
+    }
+
     // MQTT 데이터에서 들어오는 system_date의 날짜 형식은 "EEE MMM dd HH:mm:ss yyyy" 입니다.
     // 이 String 타입 날짜 데이터를 "년-월-일T시-분-초"의 LocalDateTime으로 변환해서 엔티티화 합니다.
     @RabbitListener(queues = "q.frame")
@@ -35,8 +45,10 @@ public class RabbitTopicListener {
             event = recycleFn.getEntity(recycleFn.getEntityCount());
         } catch (Exception e) {
             log.error("DATA-001 : 엔티티 조회 실패");
+            throw new CommonException("Listener-Entity", HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+        // 운영 시간
         String openTime = event.getOpenTime();
         String closeTime = event.getCloseTime();
 
@@ -64,6 +76,8 @@ public class RabbitTopicListener {
         // 날짜, 운영시간 검증, 현재 Entity와 이벤트로 넘어온 년월일이, 현재 시간과 맞는지 검증
         recycleFn.validateOperatingStatus(entityYMDDate, eventYMDDate, open, close, eventDateTime, openTime, closeTime, event);
 
+        recycleFn.validateOccupancy(event);
+
         // 이벤트로 넘어온 데이터의 Direction 가져오기
         List<String> directions = message.getEvents().stream().map(it -> it.getExtra().getCrossing_direction()).toList();
 
@@ -72,34 +86,93 @@ public class RabbitTopicListener {
             if (direction.equalsIgnoreCase("down")) {
                 event.setInCount(event.getInCount() + 1);
                 log.info("입장");
-                requestApi(event); // Request Door API
+//                requestApi(event); // Request Door API
+                recycleFn.validateOccupancy(event);
+                event.setOccupancy(event.getInCount() - event.getOutCount());
+                recycleFn.autoUpdateStatus(event);
+                eventRepository.save(event);
+                template.convertAndSend("/count/data", event);
+
             } else if (direction.equalsIgnoreCase("up")) {
                 event.setOutCount(event.getOutCount() + 1);
                 log.info("퇴장");
-            }
-
-            if (event.getInCount() - event.getOutCount() < 0) {
                 recycleFn.validateOccupancy(event);
+                event.setOccupancy(event.getInCount() - event.getOutCount());
+                recycleFn.autoUpdateStatus(event);
+                eventRepository.save(event);
+                template.convertAndSend("/count/data", event);
             }
-
-            event.setOccupancy(event.getInCount() - event.getOutCount());
-            log.info("재실 인원/최대인원 : {}명/{}명", event.getOccupancy(), event.getMaxCount());
-
-            recycleFn.autoUpdateStatus(event);
-
-            eventRepository.save(event);
-
-            // Web Socket Session 에 Event 객체 전달
-            template.convertAndSend("/count/data", event);
         }
     }
 
     // Door API에 HTTP Request 요청
-    public void requestApi(Event event) {
-        // URL 설정
-        String url = event.getRelayUrl();
+//    public void requestApi(Event event) {
+//        // URL 설정
+//        String url = event.getRelayUrl();
+//
+//        // 요청 보내기
+//        restTemplate.getForEntity(url, Void.class);
+//    }
 
-        // 요청 보내기
-        restTemplate.getForEntity(url, Void.class);
+    public void addData() throws Exception {
+
+        // 테이블에 데이터 수 확인
+        long objectCount = recycleFn.getEntityCount();
+
+        // DB에 데이터가 1개 이상이고, 그 데이터의 현재 년월일이 현재 년월일과 맞지 않으면 새로운 객체 생성
+        if (objectCount > 0) {
+            log.info("Event Table 내부에 데이터가 있습니다. 객체의 날짜가 현재 날짜와 동일한 지 검증 중...");
+
+            Event storedEvent = null;
+            try {
+                storedEvent = eventRepository.findById(recycleFn.getEntityCount()).orElse(null);
+            } catch (Exception e) {
+                log.error("기존에 존재하는 데이터 조회 실패 - Event ID : {}", storedEvent.getId(), e);
+                throw new CommonException("INIT-002", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+            if (!storedEvent.getEventTime().format(formatter).equals(currentDate.toString())) {
+                log.info("이미 생성된 데이터 내부의 날짜와 현재 날짜 비교 중 ... Event ID: {}", storedEvent.getId());
+                log.info("현재 날짜 : {} - 데이터 날짜 : {}", currentDate, storedEvent.getEventTime());
+                Event event = Event.createOf(LocalDateTime.now().truncatedTo(java.time.temporal.ChronoUnit.SECONDS));
+
+                try {
+                    event.setRelayUrl("http://192.168.0.7/index.html?p0=1000");
+                    eventRepository.save(event);
+                    log.info("기존 데이터의 날짜와 현재 시간이 불일치합니다, 새로운 객체를 생성 합니다. - Event ID: {}", event.getId());
+                } catch (Exception e) {
+                    log.error("기존 데이터의 날짜와 현재 시간 불일치합니다, - 객체 생성 실패", e);
+                    throw new CommonException("INIT-003", HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            } else {
+                log.info("현재 날짜의 데이터가 이미 존재합니다, 객체 생성 중지 - 현재 데이터의 날짜 : {}, ID : {}", storedEvent.getEventTime().format(formatter), storedEvent.getId());
+            }
+        }
+    }
+
+    // Spring 서버 재시작 할때, DB에 데이터가 하나도 없을때 초기 데이터 1개 생성
+    public void createEntity() throws Exception {
+        // 테이블에 데이터 수 확인
+        long objectCount = recycleFn.getEntityCount();
+
+        // DB에 데이터가 하나도 없으면 초기 데이터 생성
+        if (objectCount == 0) {
+            log.info("Event Table 내부에 데이터가 없습니다. 객체를 생성합니다.");
+
+            Event event = Event.createOf(LocalDateTime.now().truncatedTo(java.time.temporal.ChronoUnit.SECONDS));
+            event.setRelayUrl("http://192.168.0.7/index.html?p0=1000");
+
+            try {
+                eventRepository.save(event);
+                log.info("객체 생성 완료, Event ID: {}", event.getId());
+            } catch (Exception e) {
+                log.error("객체 생성 실패", e);
+                throw new CommonException("INIT-001", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            addData();
+        }
     }
 }
